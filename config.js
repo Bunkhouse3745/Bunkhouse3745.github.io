87e5a6c37f4ac3ed387b8f13952f4e6252831b06
Bunkhouse3745 <Bunkhouse3745@proton.me>
2023-08-22 22:54:18 -0400

    init commit

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movie-web.bunkhouse3745.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkY2FiZmMzNGI5OTNiNjQ5N2MxMWZhOGQxODdjMjlmNiIsInN1YiI6IjY0ZTU2MGRjZTg5NGE2MDEzYmFlZGI2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.y9k0iQT47SdfgnVWoqoaE5prxQFdFOz82hKA-Cuz9tY"
+};
